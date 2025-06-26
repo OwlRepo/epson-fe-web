670307d1cb0199b10d288a1ffde259fdd242c318
kenneth <kennethsevilla98@gmail.com>
2025-06-26 13:39:11 +0800

    reapply env on runtime

--- src/utils/env.ts
+++ src/utils/env.ts
@@ -1,23 +1,27 @@
 declare global {
   interface Window {
-    env: Record<string, string>;
+    ENV: Record<string, string>;
   }
 }
 
 export const getEnvVar = (key: string): string => {
-  return import.meta.env[key] || "";
+  // In development, use import.meta.env
+  if (import.meta.env.DEV) {
+    return import.meta.env[key] || "";
+  }
+
+  // In production, use window.env
+  return window.ENV?.[key] || "";
 };
 
 // Create specific getters for each env variable
-export const getApiSocketBaseUrl = () =>
-  `${getEnvVar("VITE_API_SOCKET_URL")}`;
-export const getApiRESTBaseUrl = () =>
-  `${getEnvVar("VITE_API_REST_URL")}`;
+export const getApiSocketBaseUrl = () => `${getEnvVar("VITE_API_SOCKET_URL")}`;
+export const getApiRESTBaseUrl = () => `${getEnvVar("VITE_API_REST_URL")}`;
 export const getValidUserID = () =>
   getEnvVar("VITE_VALID_USER_CARD_ID").split("|") ?? [];
 export const getUHFDeviceID = () => getEnvVar("VITE_UHF_DEVICE_ID");
 export const getUHFProductID = () => getEnvVar("VITE_UHF_PRODUCT_ID");
-export const getUHFLength = ()=> parseInt(getEnvVar('VITE_UHF_LENGTH'));
-export const getMIFARELength = ()=>parseInt(getEnvVar('VITE_MIFARE_LENGTH'));
-export const getEMLength = ()=> parseInt(getEnvVar('VITE_EM_LENGTH'));
+export const getUHFLength = () => parseInt(getEnvVar("VITE_UHF_LENGTH"));
+export const getMIFARELength = () => parseInt(getEnvVar("VITE_MIFARE_LENGTH"));
+export const getEMLength = () => parseInt(getEnvVar("VITE_EM_LENGTH"));
 // Add other env variable getters as needed
