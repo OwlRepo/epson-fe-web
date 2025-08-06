5720588a81a8901528cfe3e3118e44d83b3286b3
OwlRepo <romeoangeles010517@gmail.com>
2025-08-06 22:11:39 +0800

    fix: update API endpoint to use employeeNo instead of employeeID for fetching employee data

--- src/hooks/query/useGetEmployeeById.ts
+++ src/hooks/query/useGetEmployeeById.ts
@@ -4,7 +4,7 @@ import { useQuery } from "@tanstack/react-query";
 const getEmployeeByNo = async (employeeNo: string) => {
   try {
     const response = await api.get(
-      `api/employees/getByEmployeeID/${employeeNo}`
+      `api/employees/getByEmployeeNo/${employeeNo}`
     );
     return response.data;
   } catch (error) {
