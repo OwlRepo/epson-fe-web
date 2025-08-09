e4453b0d86c99c8d41b921f248113b36dad6caec
OwlRepo <romeoangeles010517@gmail.com>
2025-08-09 21:01:07 +0800

    fix: update authentication route to use POST method for user validation

--- src/routes/_authenticated/route.tsx
+++ src/routes/_authenticated/route.tsx
@@ -5,12 +5,7 @@ import { toast } from "sonner";
 export const Route = createFileRoute("/_authenticated")({
   component: RouteComponent,
   loader: async ({ location }) => {
-    // beforeLoad: async ({ location }) => {
-    // if (!localStorage.getItem("token") || !localStorage.getItem("user")) {
-    //   throw redirect({ to: "/", search: { redirect: location.href } });
-    //   throw redirect({ to: "/" });
-    // }
-    const { data } = await api.get(`/api/users/validate`);
+    const { data } = await api.post(`/api/users/validate`);
     if (!data.success) {
       toast.error(data.message);
       localStorage.clear();
