1bf90508e0802c0ce77cc258d06130ed328b01e2
OwlRepo <romeoangeles010517@gmail.com>
2025-08-18 00:35:07 +0800

    fix: update authentication route to ensure proper token validation and refresh handling

--- src/routes/_authenticated/route.tsx
+++ src/routes/_authenticated/route.tsx
@@ -1,22 +1,23 @@
+import api from "@/config/axiosInstance";
 import { createFileRoute, Outlet } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/_authenticated")({
   component: RouteComponent,
   loader: async () => {
-    // await api.post(`/api/users/validate`).catch(async () => {
-    //   const { data: refreshData } = await api
-    //     .post(`/api/users/refresh-token`, {
-    //       refreshToken: localStorage.getItem("refreshToken"),
-    //     })
-    //     .catch(() => {
-    //       return {
-    //         data: { success: false, message: "Invalid token" },
-    //         status: 401,
-    //       };
-    //     });
-    //   localStorage.setItem("token", refreshData.data.token);
-    //   localStorage.setItem("refreshToken", refreshData.data.refreshToken);
-    // });
+    await api.post(`/api/users/validate`).catch(async () => {
+      const { data: refreshData } = await api
+        .post(`/api/users/refresh-token`, {
+          refreshToken: localStorage.getItem("refreshToken"),
+        })
+        .catch(() => {
+          return {
+            data: { success: false, message: "Invalid token" },
+            status: 401,
+          };
+        });
+      localStorage.setItem("token", refreshData.data.token);
+      localStorage.setItem("refreshToken", refreshData.data.refreshToken);
+    });
   },
 });
 
