6f35da77165e979415388733434d1606cb179688
OwlRepo <romeoangeles010517@gmail.com>
2025-08-18 22:50:05 +0800

    fix: update authentication route to dynamically handle API endpoints for session validation and token refresh

--- src/routes/_authenticated/route.tsx
+++ src/routes/_authenticated/route.tsx
@@ -1,24 +1,25 @@
-// import api from "@/config/axiosInstance";
+import api from "@/config/axiosInstance";
+import { getIsEVS } from "@/utils/env";
 import { createFileRoute, Outlet } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/_authenticated")({
   component: RouteComponent,
-  // loader: async () => {
-  //   await api.post(`/api/users/validate`).catch(async () => {
-  //     const { data: refreshData } = await api
-  //       .post(`/api/users/refresh-token`, {
-  //         refreshToken: localStorage.getItem("refreshToken"),
-  //       })
-  //       .catch(() => {
-  //         return {
-  //           data: { success: false, message: "Invalid token" },
-  //           status: 401,
-  //         };
-  //       });
-  //     localStorage.setItem("token", refreshData.data.token);
-  //     localStorage.setItem("refreshToken", refreshData.data.refreshToken);
-  //   });
-  // },
+  loader: async () => {
+    await api.post(`/api/${getIsEVS() ? "evs" : "users"}/validate`).catch(async () => {
+      const { data: refreshData } = await api
+        .post(`/api/${getIsEVS() ? "evs" : "users"}/refresh-token`, {
+          refreshToken: localStorage.getItem("refreshToken"),
+        })
+        .catch(() => {
+          return {
+            data: { success: false, message: "Invalid token" },
+            status: 401,
+          };
+        });
+      localStorage.setItem("token", refreshData.data.token);
+      localStorage.setItem("refreshToken", refreshData.data.refreshToken);
+    });
+  },
 });
 
 function RouteComponent() {
