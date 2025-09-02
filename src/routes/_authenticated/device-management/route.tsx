7d5d96f45ed7c3f2fd51cf6c44c79283b27f7eea
OwlRepo <romeoangeles010517@gmail.com>
2025-09-03 02:48:17 +0800

    feat: implement redirection logic in device management route to ensure users are directed to the overview page when accessing the dashboard

--- src/routes/_authenticated/device-management/route.tsx
+++ src/routes/_authenticated/device-management/route.tsx
@@ -1,12 +1,24 @@
 import { DeviceManagementLayout } from "@/components/layouts/DeviceManagementLayout";
 import { withModuleAccess } from "@/utils/guardRoute";
-import { createFileRoute, Outlet } from "@tanstack/react-router";
+import { createFileRoute, Outlet, redirect } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/_authenticated/device-management")({
   component: RouteComponent,
-  beforeLoad: withModuleAccess(["DMG"], {
-    fallbackError: new Error("You do not have access to this module."),
-  }),
+  beforeLoad: ({ location }) => {
+    withModuleAccess(["DMG"], {
+      fallbackError: new Error("You do not have access to this module."),
+    });
+
+    if (
+      location.pathname === "/device-management" ||
+      location.pathname === "/device-management/dashboard"
+    ) {
+      throw redirect({
+        to: "/device-management/dashboard/overview",
+        replace: true,
+      });
+    }
+  },
 });
 
 function RouteComponent() {
