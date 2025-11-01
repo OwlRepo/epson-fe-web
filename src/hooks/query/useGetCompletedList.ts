d835720ce77e0763a67922ae868b978987c02c64
OwlRepo <romeoangeles010517@gmail.com>
2025-11-01 21:37:26 +0800

    refactor: clean up useGetCompletedList hook by removing commented code and improving error logging

--- src/hooks/query/useGetCompletedList.ts
+++ src/hooks/query/useGetCompletedList.ts
@@ -3,18 +3,11 @@ import { useQuery } from "@tanstack/react-query";
 
 const getCompletedList = async () => {
   try {
-    // let data;
     const response = await api.get(`api/evs/getCompletedList`);
 
-    // if (Array.isArray(response?.data?.data)) {
-    //   data = response.data.data.map((item: any) => ({
-    //     label: item.Type,
-    //     value: item.Type,
-    //   }));
-    // }
     return response.data.data;
   } catch (error) {
-    console.error("Error fetching department data:", error);
+    console.error("Error fetching completed list data:", error);
   }
 };
 
