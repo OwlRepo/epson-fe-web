c7fadacd0ff4b3eda7d889c799fd9ff9d3fd3789
OwlRepo <romeoangeles010517@gmail.com>
2025-11-15 01:35:18 +0800

    fix: add API call to validate user token after successful refresh for improved authentication flow

--- src/routes/_authenticated/route.tsx
+++ src/routes/_authenticated/route.tsx
@@ -4,26 +4,30 @@ import { createFileRoute, Outlet, redirect } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/_authenticated")({
   component: RouteComponent,
-  loader: async () => {
-    await api
-      .post(`/api/${getIsEVS() ? "evs" : "users"}/validate`)
-      .catch(async () => {
-        const { data: refreshData, status } = await api
-          .post(`/api/${getIsEVS() ? "evs" : "users"}/refresh-token`, {
-            refreshToken: localStorage.getItem("refreshToken"),
-          })
-          .catch(() => {
-            localStorage.clear();
-            throw redirect({
-              to: "/",
-              replace: true,
+  loader: () => {
+    const validateSession = async () => {
+      await api
+        .post(`/api/${getIsEVS() ? "evs" : "users"}/validate`)
+        .catch(async () => {
+          const { data: refreshData, status } = await api
+            .post(`/api/${getIsEVS() ? "evs" : "users"}/refresh-token`, {
+              refreshToken: localStorage.getItem("refreshToken"),
+            })
+            .catch(() => {
+              localStorage.clear();
+              throw redirect({
+                to: "/",
+                replace: true,
+              });
             });
-          });
-        if (status === 200 && refreshData.success) {
-          localStorage.setItem("token", refreshData.data.token);
-          localStorage.setItem("refreshToken", refreshData.data.refreshToken);
-        }
-      });
+          if (status === 200 && refreshData.success) {
+            localStorage.setItem("token", refreshData.data.token);
+            localStorage.setItem("refreshToken", refreshData.data.refreshToken);
+            validateSession();
+          }
+        });
+    };
+    validateSession();
   },
 });
 
